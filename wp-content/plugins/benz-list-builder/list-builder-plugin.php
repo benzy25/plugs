98089ac62df311b4ce99ddd4248d569eb2f71bbc
Benzy25 <benz_rob@yahoo.com>
2016-01-30 11:54:02 -0500

    check diff

--- wp-content/plugins/benz-list-builder/list-builder-plugin.php
+++ wp-content/plugins/benz-list-builder/list-builder-plugin.php
@@ -9,13 +9,57 @@
  * License: GPL2
  */
 
-// registers all our custom shorcodes on init
+/* !0. TABLE OF CONTENTS */
+
+/*
+	
+	1. HOOKS
+		1.1 - registers all our custom shortcodes
+        1.2 - register custom admin column headers
+	
+	2. SHORTCODES
+		2.1 - slb_register_shortcodes()
+		2.2 - slb_form_shortcode()
+		
+	3. FILTERS
+        3.1 - creating custom column header data
+		
+	4. EXTERNAL SCRIPTS
+		
+	5. ACTIONS
+		
+	6. HELPERS
+		
+	7. CUSTOM POST TYPES
+	
+	8. ADMIN PAGES
+	
+	9. SETTINGS
+
+*/
+
+
+/* !1. HOOKS */
+
+// 1.1
+// hint: registers all our custom shortcodes on init
 add_action('init', 'benz_register_shortcodes');
 
+//1.2
+//hint: register custom admin column headers
+add_filter('manage_edit-benz_subscriber_columns', 'benz_subscriber_column_headers');
+
+add_filter('manage_edit-slb_subscriber_columns','slb_subscriber_column_headers');
+
+/* !2. SHORTCODES */
+// 2.1
+// hint: registers all our custom shortcodes
 function benz_register_shortcodes() {
     add_shortcode('benz_form', 'benz_form_shortcode');
 }
 
+// 2.2
+// hint: returns a html string for a email capture form
 function benz_form_shortcode( $args, $content="") {
     // Setup output var - form html
     $output = '
@@ -32,12 +76,13 @@ function benz_form_shortcode( $args, $content="") {
         <label>Your Email</label></br />
         <input type="email" name="benz_email" placeholder="you@email.com" />
         </p>';
+    	// including content in our form html if content is passed into the function
     if( strlen($content) ):
 
         $output .= '<div class="benz-content">' . wpautop($content) . '</div>';
 
         endif;
-
+// completing our form html
     $output .='<p class="benz-input-wrap">
         <input type="submit" name="benz_submit" value="Sign Up!" />
         </p>
@@ -49,3 +94,44 @@ function benz_form_shortcode( $args, $content="") {
     return $output;
 
 }
+
+/* !3. FILTERS */
+//3.1
+function benz_subscriber_column_headers( $columns ) {
+    
+    //creating custom column header data override settings
+    $columns = array(
+    'cb'    => '<input type="checkbox" />',
+    'title' => __('Subscriber Name'),
+    'email' => __('Email Address')
+    );
+}
+return $columns;
+
+
+/* !4. EXTERNAL SCRIPTS */
+
+
+
+
+/* !5. ACTIONS */
+
+
+
+
+/* !6. HELPERS */
+
+
+
+
+/* !7. CUSTOM POST TYPES */
+
+
+
+
+/* !8. ADMIN PAGES */
+
+
+
+
+/* !9. SETTINGS */
