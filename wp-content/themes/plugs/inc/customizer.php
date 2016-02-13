fc59081aa6694d7e27e21562c860aa3d83e51460
Benzy25 <benz_rob@yahoo.com>
2016-02-13 13:43:35 -0500

    added new start theme

---
+++ wp-content/themes/plugs/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * PLUGS Theme Customizer.
+ *
+ * @package PLUGS
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function plugs_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'plugs_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function plugs_customize_preview_js() {
+	wp_enqueue_script( 'plugs_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'plugs_customize_preview_js' );
