fa66d2085805f515f3b5985604e1a6f95343dcb2
Benzy25 <benz_rob@yahoo.com>
2017-07-22 07:49:33 -0400

    before theme fucking

--- wp-content/themes/bst/functions.php
+++ wp-content/themes/bst/functions.php
@@ -210,8 +210,6 @@ class RRHE {
 new RRHE();
 
 
-
-
 // -- woocommerce support to theme
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
